3bcfa5728eac037daae86ef359b2403b60c015c3
Milena <milenalebedin@gmail.com>
2021-09-22 00:09:42 -0300

    Clase 03 ej 3.15 y 3.16 tabla_informe.py

--- Clase03/tabla_informe.py
+++ Clase03/tabla_informe.py
@@ -1,44 +1,68 @@
-"los paths se utilizaron en función de que los archivos se encuentran en la misma carpeta en la que se encuentra el código"
-
 import csv
 
-def calcular_costo_camion(nombre_archivo):
-    costo_total = 0
+
+def calcular_costo_camion(nombre_archivo, header = True) -> list[dict]:
+    """"
+    parametros:
+    nombre_archivo, path del archivo con datos de cajones: Producto, cantidad, Precio
+    header, indica si el archivo tiene encabezado
+    devuelve:
+    camion, una lista de tuplas con los datos del cajon mas el costo total de cada cajon
+    """
+    camion = []
     with open(nombre_archivo, 'rt') as file:
         rows = csv.reader(file)
-        keys = next(rows)
+        # El encabezado esta fijado
+        if(header):
+            next(rows)
         for row in rows:
             try:
-                record = dict(zip(keys, row))
-                ncajones = int(record['cajones'])
-                precio = float(record['precio'])
-                costo_total += ncajones * precio
-                values = (row[0], int(row[1]), float(row[2]))
+                nombre = row[0]
+                ncajones = float(row[1])
+                precio_unidad = float(row[2])
+                costo_total = ncajones * precio_unidad
+                values = (nombre, ncajones, precio_unidad, costo_total)
+                camion.append(values)
             except ValueError:
+                # Sería interesante agregar un contador de filas que se han salteado por Excepción
                 pass
-            except TypeError:
-                pass
-    return costo_total
+    return camion
 
-def leer_precios(file_name):
+def leer_precios(file_name: str) -> dict:
+    """
+    lee un csv con dos columnas y devuelve un diccionario de la forma
+    col0: col1
+    """
     precios_dict = {}
     with open(file_name, 'rt') as file:
         rows = csv.reader(file)
         for row in rows:
             try:
-                dict.update(precios_dict, {row[0]: row[1]})
+                dict.update(precios_dict, {str(row[0]): float(row[1])})
             except IndexError:
                 pass
     return precios_dict
 
-costo_camion = calcular_costo_camion('fecha_camion.csv')
-print(costo_camion)
+
+
+def hacer_informe(camion_costo, precios):
+    """
+    camion_costo: lista de diccionarios
+    """
+    informe = []
+    for cajon in camion_costo:
+        balance_unidad = -cajon[2] + precios[cajon[0]]  
+        informe.append((cajon[0], cajon[1], cajon[2], balance_unidad))
+    return informe
+
+
+camion_costo = calcular_costo_camion('camion.csv')
 precios = leer_precios('precios.csv')
-
-pago = 0
-costo = 0
-for c in costo_camion:
-    pago += c['cajones'] * c['precio']
-    costo += float(precios[c['nombre']])
-
-print('Balance: ', pago-costo)
+print(type(precios))
+informe = hacer_informe(camion_costo, precios)
+headers = ('Nombre', 'Cajones', 'Precio', 'Cambio')
+separator = ('--------- ---------- ---------- ----------')
+print('%10s %10s %10s %10s' % headers)
+print(separator)
+for r in informe:
+    print('%10s %10d %10.2f %10.2f' % r)
